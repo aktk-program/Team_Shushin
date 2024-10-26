6c34631c8a3736d306849c1e3d72addb0e223864
wotsu <y1u0s2i7@gmail.com>
2024-10-26 18:40:25 +0900

    採点機能の修正

--- back/main.py
+++ back/main.py
@@ -4,8 +4,10 @@ app = FastAPI()
 
 import string_to_model
 import reset_recorded_foods
+import scoring
 
 app.include_router(string_to_model.router)
 app.include_router(reset_recorded_foods.router)
+app.include_router(scoring.router)
 
 # uvicorn main:app --reload で実行
